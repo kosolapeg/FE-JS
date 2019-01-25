4bad5094b9a468561f89b586ddb639c379fc1af2
gordkat <46678529+gordkat@users.noreply.github.com>
2019-01-25 23:40:48 +0100

    Add files via upload

--- module02/js/script.js
+++ module02/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+let userInput = 0;
+const numbers = [];
+let total = 0;
+
+do {
+  userInput = prompt('Введите число');
+  console.log(userInput);
+  if (userInput === null) {
+    break;
+  } else {
+    let validNumber = Number(userInput);
+
+    if (Number.isNaN(validNumber)) {
+      alert('Было введено не число, попробуйте еще раз');
+    } else {
+      numbers.push(validNumber);
+    }
+  }
+} while (userInput !== null);
+
+if (numbers.length > 0) {
+  for (const value of numbers) {
+    total = total + value;
+  }
+}
+alert(`Общая сумма чисел равна ${total}`);
